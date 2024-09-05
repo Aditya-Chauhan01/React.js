d6812c44d22a2837c7e928abf663ba2ec2364263
Aditya-Chauhan01 <adityachauhan10201@gmail.com>
2024-09-05 19:12:24 +0530

    modified

--- 02 Hooks/src/App.jsx
+++ 02 Hooks/src/App.jsx
@@ -7,8 +7,16 @@ function App() {
   const[count, setCount] = useState(0)    // Hooks(UI par show krane ke liye use kiya jata hai), jahan jahan par count hoga vahan par value inject ho jayegi or setcount usko new value de rha hai
 
   const addValue = () => {
+    // setCount(count+1)   // value get as bundle of package for count. each line the state is set in 0
     // setCount(count+1)
-    setCount(previousCount => previousCount+1)
+    // setCount(count+1)
+    // setCount(count+1)
+
+
+    setCount((previousCount) => previousCount+1)  // in setcounter we get a call back function
+    setCount((previousCount) => previousCount+1)  // excuting a fun then value of count is update
+    setCount(previousCount => previousCount+1)  
+    setCount(previousCount => previousCount+1)  
   }
 
   const removeValue = () => {
